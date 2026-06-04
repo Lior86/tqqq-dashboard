1d1da9984c6d240eb669093ea7d5b89d047faa0c
Lior Naor <lnaor86@gmail.com>
2026-06-04 12:53:31 -0400

    Remove 1D timeframe

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ APP_ICON = "📈"
 # Timeframe options shown as buttons in the UI
 # Key = label on the button, Value = how far back yfinance fetches
 TIMEFRAMES = {
-    "1D":  "1d",
     "1W":  "5d",
     "1M":  "1mo",
     "3M":  "3mo",
